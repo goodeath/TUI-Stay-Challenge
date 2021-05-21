75343a4a3ebd9072ed5ca96371f0399e9779eed3
Alex Anderson <alexandersonmbm@gmail.com>
2021-05-21 11:37:44 -0300

    feat: configure express app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import helmet from 'helmet';
+import { connectToDb } from './config/database';
+import { HotelOfferRouter } from './routers/hotelOfferRouter';
+import { HotelRouter } from './routers/hotelRouter';
 
 const PORT = 4000;
 const app = express();
 
-app.listen(PORT, () => {
+app.use(helmet());
+
+app.use('/hotels', HotelRouter);
+app.use('/offers', HotelOfferRouter);
+
+app.listen(PORT, async () => {
+    await connectToDb();
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
