3ab0a15899ba920f4ff8c79dd841ebaccddf7ea0
Alex Anderson <alexandersonmbm@gmail.com>
2021-05-21 11:29:31 -0300

    feat(database): create db connection file

---
+++ src/config/database.ts
@@ -0,0 +1,13 @@
+import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+export const connectToDb = () => {
+    connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:27017/${DB_NAME}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        authSource: 'admin',
+    });
+};
